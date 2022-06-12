83ce1b989460f7df05970ce0e01c0fedeb71a954
Venkatarama Avadhani <51362740+vavadhani@users.noreply.github.com>
2022-06-12 18:24:28 +0530

    Update init.el
    Remove duplicate lsp-ui and lsp-mode

--- init.el
+++ init.el
@@ -152,26 +152,6 @@
   (lsp-ui-sideline-show-hover t)
   (lsp-ui-doc-enable nil))
 
-(use-package lsp-mode
-  :ensure
-  :commands lsp
-  :custom
-  ;; what to use when checking on-save. "check" is default, I prefer clippy
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  :config
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-(use-package lsp-ui
-  :ensure
-  :commands lsp-ui-mode
-  :custom
-  (lsp-ui-peek-always-show t)
-  (lsp-ui-sideline-show-hover t)
-  (lsp-ui-doc-enable nil))
-
 (use-package company
   :ensure
   :custom
